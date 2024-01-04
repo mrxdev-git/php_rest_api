1f8dc80151fde2b8b976a5dbed4d8cb0d8e143b9
Mr X <nertic.mihai@gmail.com>
2024-01-04 13:09:21 +0200

    Products retrieval logic added

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,10 +2,16 @@
 
 namespace DataEx\Controller;
 
+use DataEx\Model\ProductsModel;
+
 class ProductsController extends Controller
 {
 	public function process(): array
 	{
+		$offset = $_GET['offset'] ?? 0;
+		$limit  = $_GET['limit'] ?? 100;
 
+		$model = new ProductsModel();
+		return $model->getAll($offset, $limit);
 	}
 }
