cb0a373d0bf2efc64686eb386b9bd8a75ed5ddf2
Mr X <nertic.mihai@gmail.com>
2024-01-10 23:17:12 +0200

    Features controller completed

--- src/Controller/FeaturesController.php
+++ src/Controller/FeaturesController.php
@@ -2,14 +2,20 @@
 
 namespace DataEx\Controller;
 
+use DataEx\Model\FeaturesModel;
+
 class FeaturesController extends Controller
 {
 
 	public function process(): array
 	{
 
+		$offset = $_GET['offset'] ?? 0;
+		$limit = $_GET['limit'] ?? 100;
 
+		$model = new FeaturesModel();
 
+		return $model->getFeatures($offset,$limit);
 	}
 
 }
