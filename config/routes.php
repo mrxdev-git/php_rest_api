fd835a81e22832e56bda818f29810979c4a30154
Mr X <nertic.mihai@gmail.com>
2024-01-10 13:31:51 +0200

    Routes added

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,21 @@
 <?php
 
 use \DataEx\Routing\Router;
+use \DataEx\Controller\CategoriesController;
+use \DataEx\Controller\ProductsController;
+use \DataEx\Controller\FiltersController;
 
-Router::get('/categories', []);
-Router::get('/products', []);
+Router::get('/categories', [
+	   CategoriesController::class
+]);
+
+Router::get('/products', [
+	   ProductsController::class
+]);
+
+Router::get('/filters', [
+	   FiltersController::class
+]);
 Router::get('/filters', []);
 
 
