be412ab68ea06de91a75ed24a66f07425e917a7e
Mr X <nertic.mihai@gmail.com>
2024-01-10 23:16:54 +0200

    Getting features method added

--- src/Model/FeaturesModel.php
+++ src/Model/FeaturesModel.php
@@ -4,4 +4,13 @@ namespace DataEx\Model;
 
 class FeaturesModel extends Model {
 
+	public function getFeatures($offset, $limit)
+	{
+		return $this->getAll(
+			   '*',
+			   'id ASC',
+			   $offset,
+			   $limit
+		);
+	}
 }
